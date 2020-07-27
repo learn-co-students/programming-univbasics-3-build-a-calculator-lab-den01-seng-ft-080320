eb63e43bdd703cd986ea17b5eb5bb30c00cb111b
Music Meier <musicm987@gmail.com>
2020-07-27 06:43:10 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  difference = (num1 - num2)
+  return difference  
 end
 
 def division(num1, num2)
-
+  divided_number = num1/num2
+  return divided_number
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1*num2
+  return multiplication
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  return remainder
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  return root
 end
